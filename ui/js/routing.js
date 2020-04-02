483c4312b6f734562e2f75518d1a3946277457c5
czaid <czaid@Laptop>
2020-04-02 03:33:04 +0200

    UI standardization

--- ui/js/routing.js
+++ ui/js/routing.js
@@ -1,25 +1,26 @@
-$(document).ready(function() {
-	$(window).on('hashchange', function(e) {
-		// e.preventDefault();
-		
-		// Constants
-		var PLACEHOLDER_DIV_NAME = "#content";
-		var PARTIALS_PATH = "partials/";
-		var PARTIALS_EXT = ".html"
-		var PARTIALS_DEFAULT_PAGE = "default"
-			
-		// By default map URL's anchors to the same name as HTML file
-		var anchor = window.location.hash;
-		$(PLACEHOLDER_DIV_NAME).load(PARTIALS_PATH + anchor.replace('#', '') + PARTIALS_EXT);
-		
-		// Custom overrides + Default route
-		switch (anchor) {
-		case "#unusual":
-			$(PLACEHOLDER_DIV_NAME).load("partials/test.html");
-			break;
-		default
-			$(PLACEHOLDER_DIV_NAME).load(PARTIALS_PATH + PARTIALS_DEFAULT_PAGE + PARTIALS_EXT);
-			break;
-		}
-	});
+
+function route() {
+	var anchor = window.location.hash.replace("#", "");
+
+	// Custom overrides	
+	switch (anchor) {
+	case "unusual":
+		anchor = "test";
+		break;
+	}
+	
+	// Load HTML partial
+	// If no overrides, the partial with the same name as the anchor
+	$("#content").load("partials/" + anchor + ".html");
+}
+
+// If no anchor, add one
+if (!window.location.hash) {
+	window.location += "#home";
+} else {
+	route();
+}
+// Route to partial each time the anchor changes
+$(window).on("hashchange", function(e) {
+	route();
 });
