1c1ac202998a8df4d2349b5e18f1a5673f62539b
czaid <czaid@Laptop>
2020-04-03 13:03:06 +0200

    Few Pages init

--- api/src/main/java/ma/simpleit/tabdyl/domain/User.java
+++ api/src/main/java/ma/simpleit/tabdyl/domain/User.java
@@ -19,6 +19,7 @@ import ma.simpleit.tabdyl.domain.enumeration.IdDocumentType;
 @EqualsAndHashCode(callSuper=true)
 public class User extends BaseEntity {
 	
+	private String username;
 	private String email;
 	private String password;
 	private String firstname;
